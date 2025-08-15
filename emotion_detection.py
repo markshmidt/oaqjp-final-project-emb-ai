21d8be7c6cb0171808834a9f26fac45c594f1211
Mariia <masha.shmidt.04@gmail.com>
2025-08-15 17:45:47 -0400

    commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -13,4 +13,29 @@ def emotion_detector(text_to_analyze):
         }
     }
     response = requests.post(url, headers=headers, json=input_json)
-    return response.text
+    result = json.loads(response.text)
+    
+    emotions = result.get("emotionPredictions", [])[0].get("emotion", {})
+    
+    anger_score = emotions.get("anger", 0)
+    disgust_score = emotions.get("disgust", 0)
+    fear_score = emotions.get("fear", 0)
+    joy_score = emotions.get("joy", 0)
+    sadness_score = emotions.get("sadness", 0)
+    
+    scores = {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score
+    }
+    dominant_emotion = max(scores, key=scores.get)
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion
+    }
